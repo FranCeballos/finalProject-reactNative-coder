6c6aa94219e0535042baee0dc993bdd89a3aad4a
Francisco Ceballos <ceballosfranciscomartin@gmail.com>
2023-07-13 10:04:29 -0300

    Update index.js

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -15,7 +15,7 @@ const NavBar = ({ onDeleteAll }) => {
       }}
     >
       <Text style={styles.emptyLeftText}></Text>
-      <Text style={styles.title}>MyApp</Text>
+      <Text style={styles.title}>MyToDoApp</Text>
       <View style={styles.clearButtonBox}>
         <View style={styles.clearButtonPadding}>
           <Button title="Clear All" color="red" onPress={onDeleteAllHandler} />
